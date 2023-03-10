83db987229ede1eb9cfa9b103e3d8fa71ec0f734
HC <harshalchavan170@gmail.com>
2023-03-10 23:49:51 +0530

    First test

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("this is me who");
+        System.out.println("Hello world!");
+        System.out.println("heyy baee");
+        System.out.println("hemlooo");
+
+
+    }
+}
